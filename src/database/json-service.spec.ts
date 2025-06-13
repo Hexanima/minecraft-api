cf9bcfdbf2f93f9b3846ac98c317ad954acfa97e
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2025-06-12 22:54:13 -0300

    Finishes Potion DB

--- src/database/json-service.spec.ts
+++ src/database/json-service.spec.ts
@@ -6,7 +6,7 @@ describe("JsonService Test", async () => {
     const ingredients = await JsonService.retrieve(JsonFiles.ingredients);
 
     expect(ingredients).toBeInstanceOf(Array);
-    expect(ingredients).toHaveLength(15);
+    expect(ingredients).toHaveLength(17);
 
     for (const ingredient of ingredients) {
       expect(ingredient).toEqual({
@@ -18,7 +18,7 @@ describe("JsonService Test", async () => {
     const potions = await JsonService.retrieve(JsonFiles.potions);
 
     expect(potions).toBeInstanceOf(Array);
-    expect(potions).toHaveLength(1);
+    expect(potions).toHaveLength(20);
 
     for (const potion of potions) {
       expect(potion).toEqual({
